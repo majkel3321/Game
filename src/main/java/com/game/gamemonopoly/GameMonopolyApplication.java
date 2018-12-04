b939fe7da08f921b812354fa2d3e9b110c11eba1
majkel3321 <majkel3321@gmail.com>
2018-12-04 13:22:39 +0100

    first commit

---
+++ src/main/java/com/game/gamemonopoly/GameMonopolyApplication.java
@@ -0,0 +1,14 @@
+package com.game.gamemonopoly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameMonopolyApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(GameMonopolyApplication.class, args);
+
+    }
+}
